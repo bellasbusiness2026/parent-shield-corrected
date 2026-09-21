7906920d494291245cb338e4c462a4710e4011c6
bellasbusiness2026 <bellasbusiness2026@gmail.com>
2026-09-21 04:13:53 -0500

    P3 copy: polish Texas deep + other-state consent rows (Theo Part 2)

--- src/data/authorities/index.ts
+++ src/data/authorities/index.ts
@@ -5,7 +5,7 @@ import { otherStateAuthorities } from './otherStates';
 
 export { federalFloorAuthorities, FEDERAL_FLOOR_DISCLAIMER } from './federalFloor';
 export { texasDeepAuthorities } from './texasDeep';
-export { otherStateAuthorities } from './otherStates';
+export { otherStateAuthorities, OTHER_STATE_RECORDING_FOOTNOTE } from './otherStates';
 
 /** All jurisdiction packs combined for the P3 state selector. */
 export const ALL_AUTHORITIES: AuthorityRow[] = [
